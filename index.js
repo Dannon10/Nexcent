20e8e5ba4be0b844107a34b900213b8ac43d2236
Dannon Abayomi <104930365+Dannon10@users.noreply.github.com>
2024-08-10 15:19:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,19 +3,7 @@
 const toggleBtn = document.querySelector('.toggle-btn');
 const toggleBtnIcon = document.querySelector('.toggle-btn i');
 const dropDownMenu = document.querySelector('.dropdown-menu');
-const bodyBlur = document.querySelector('.body-blur')
 const overlay = document.querySelector(".overlay");
-const btnCloseModal = document.querySelector('.close-modal');
-
-
-
-const closeModal = () => {
-    modal.classList.add('hidden')
-    overlay.classList.add('hidden')
-};
-
-btnCloseModal.addEventListener('click', closeModal)
-overlay.addEventListener('click', closeModal)
 
 toggleBtn.onclick = function() {
     dropDownMenu.classList.toggle('open')
